1b9cd7db49d9d1c60ca63b6ca38f72530979a40c
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-09-12 21:32:46 +0530

    Registry Chnages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Consul;
+using Ocelot.Provider.Consul; // Add this for Consul integration
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 // Register Ocelot and Consul services
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddConsul(); // AddConsul for service discovery
+
 builder.Services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(config =>
 {
-    config.Address = new Uri("http://localhost:8500");
+    config.Address = new Uri("http://localhost:8500"); // Consul agent address
 }));
 
 var app = builder.Build();
 
+// Use Ocelot middleware
 await app.UseOcelot();
 
 app.Run();
